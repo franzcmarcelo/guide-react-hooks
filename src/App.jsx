506d12d2dfef66eb69536eb107366181beb17a03
Franz <franzcmarcelo@gmail.com>
2022-10-14 03:28:06 -0500

    base `pnpm create vite` react app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <div className='container d-flex flex-column gap-5'>
+      <div>
+        <h1 className='text-center'>React Hooks</h1>
+        <hr/>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
